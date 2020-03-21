da239f8457155537ca7c26bd447560357f6b6202
Shinn Chong <shinnyx@gmail.com>
2020-03-21 17:14:52 +0800

    Simple react app to upload file to S3

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UploadComponent from './components/upload_component'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <UploadComponent></UploadComponent>
+      </div>
+    </div>
+  );
+}
+
+export default App;
