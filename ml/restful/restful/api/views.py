5dd2ea4003d30c9004e8f0fc86153e8175150bdd
anonymous <shawn.coding.acc@gmail.com>
2020-03-22 22:29:41 +0800

    S3 webhook code

--- ml/restful/restful/api/views.py
+++ ml/restful/restful/api/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+import json
+import os
+
+import boto3
+import botocore
+from django.conf import settings
 from django.contrib.auth.models import User, Group
+from django.shortcuts import render
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from restful.analytics.data_processing import *
 from restful.api.serializers import UserSerializer, GroupSerializer
 
 
@@ -21,3 +30,44 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class WebhookViewSet(APIView):
+    """
+    Webhook for uploaded file
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        filename = f"{request.GET.get('filename')}.json"
+
+        try:
+            s3 = boto3.Session(aws_access_key_id=settings.REACT_APP_AWS_ACCESS_KEY_ID,
+                               aws_secret_access_key=settings.REACT_APP_AWS_SECRET_ACCESS_KEY).resource("s3")
+            downfile = f"restful/analytics/{filename}"
+            s3.meta.client.download_file(settings.REACT_APP_S3_BUCKET, filename, downfile)
+
+            # analytics
+            print(f"Processing {downfile} ...")
+            place_visit_df, activity_segment_df = parse_google_takeout_semantic_location_history(downfile)
+
+            # filename1 = downfile.split(".json")[0] + "_place_visit.csv"
+            # place_visit_df.to_csv(filename1, index=False)
+            # filename2 = downfile.split(".json")[0] + "_activity_segment.csv"
+            # activity_segment_df.to_csv(filename2, index=False)
+            # print(f"Data exported:\n\t- {filename1}\n\t- {filename2}")
+            print("Data import and processing completed. Program terminated ...")
+
+            try:
+                os.remove(downfile)
+            except OSError:
+                pass
+
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+
+        # return Response(f"Success. {json.dumps(activity_segment_df[['startLocationlatitudeE7']].to_dict('records'))}")
+        return Response(f"Success.")
--- ml/restful/restful/settings.py
+++ ml/restful/restful/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import environ
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = '1ndebjncev8yssy-$hk@r@9zezv=x_-g5@z#*=i*^8w*f)h542'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restful.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -114,15 +111,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 # Custom
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '.env'))
+REACT_APP_AWS_ACCESS_KEY_ID = env('REACT_APP_AWS_ACCESS_KEY_ID')
+REACT_APP_AWS_SECRET_ACCESS_KEY = env('REACT_APP_AWS_SECRET_ACCESS_KEY')
+REACT_APP_S3_BUCKET = env('REACT_APP_S3_BUCKET')
+REACT_APP_S3_REGION = env('REACT_APP_S3_REGION')
