a46c462f4ad0e057a0a4150165bb7a0b6438310b
anonymous <shawn.coding.acc@gmail.com>
2020-03-23 12:57:30 +0800

    Update database models

--- ml/restful/restful/api/models.py
+++ ml/restful/restful/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Cluster(models.Model):
+    latitude = models.FloatField()
+    longtitude = models.FloatField()
+    timestamp = models.FloatField()
+    cluster = models.TextField()
+    source = models.TextField()
